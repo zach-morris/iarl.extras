15b060efc7e2fb5586e9c9970b8845685a5b3d2a
Zach Morris <ZC@Zs-MacBook-Pro.local>
2017-12-02 14:08:43 -0800

    Update initial commit 10

--- update_iarl_extras.py
+++ update_iarl_extras.py
@@ -5,7 +5,7 @@ xml_files = glob.glob(os.path.join(base_dir,'dat_files','*.xml'))
 total_lines = 500  #Read up to this many lines looking for the header
 new_line = '\r\n'
 iarl_extras_text = '<datafile>'+new_line
-iarl_extras_entry = '\t<extrafile>'+'xxheader_textxx'+'</extrafile>'+new_line
+iarl_extras_entry = '\t<extrafile>'+new_line+'\t\t<emu_extras_filename>xxxml_filenamexx</emu_extras_filename>'+'xxheader_textxx'+'</extrafile>'+new_line
 dont_include_these_lines = ['<emu_parser>','<emu_category>','<emu_author>','<emu_homepage>','<emu_baseurl>','<emu_launcher>','<emu_ext_launch_cmd>','<emu_downloadpath>','<emu_postdlaction>'] #Do not need to include this info in the summary xml
 
 for xml_file in xml_files:
@@ -33,7 +33,7 @@ for xml_file in xml_files:
 				f.close()
 				print('Unable to find the header in the xml file '+str(xml_file))
 		if header_end == 1:
-			iarl_extras_text = iarl_extras_text+iarl_extras_entry.replace('xxheader_textxx',header_text)
+			iarl_extras_text = iarl_extras_text+iarl_extras_entry.replace('xxheader_textxx',header_text).replace('xxxml_filenamexx',str(os.path.split(xml_file)[-1]))
 
 iarl_extras_text = iarl_extras_text+'</datafile>'
 iarl_extras_text = iarl_extras_text.replace('\t\t\t','\t\t').replace('\t\t</extrafile>','\t</extrafile>').replace('\t\t\t<emu','\t\t<emu')
